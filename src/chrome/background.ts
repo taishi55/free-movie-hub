b4c59589b4bd61aa77ffa30c82898baf966d941a
taishi55 <105188885+taishi55@users.noreply.github.com>
2023-11-12 14:38:27 -0800

    directly open the menu

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,6 +1,8 @@
 // write background.js
 import Browser from "webextension-polyfill";
 
-Browser.action.onClicked.addListener(async(tab) => {
-    await Browser.tabs.create({ url: Browser.runtime.getURL("index.html") });
+Browser.action.onClicked.addListener(async (tab) => {
+  await Browser.tabs.create({
+    url: "https://www.yfsp.tv/list?region=%E6%AC%A7%E7%BE%8E&language=%E8%8B%B1%E8%AF%AD&year=%E4%BB%8A%E5%B9%B4",
+  });
 });
